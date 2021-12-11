472b74ce87acb940bb2cde69900e71bde36daab3
郵差 <twHenryYang@gmail.com>
2021-12-11 16:54:14 +0800

    Finish send key function

--- readButtonState_and_sendKeyboardSignal_20211211/readButtonState_and_sendKeyboardSignal_20211211.ino
+++ readButtonState_and_sendKeyboardSignal_20211211/readButtonState_and_sendKeyboardSignal_20211211.ino
@@ -3,6 +3,7 @@
   + https://www.arduino.cc/reference/en/language/functions/usb/keyboard/keyboardwrite/
 
 */
+#include <Keyboard.h>;
 // constants won't change. They're used here to set pin numbers:
 const int buttonPin[6] = {696969, A1, A2, A3, A4, A5};
 // the number of the pushbutton pin
@@ -20,21 +21,41 @@ void setup() {
     pinMode(buttonPin[i], INPUT);
   }
   Serial.begin(9600);
+  Keyboard.begin();
 }
+
+int buttonLock[6] = {0, 0, 0, 0, 0, 0};
 
 void loop() {
   String allBtnState;
   for (int i = 1; i < 6; i++) {
     // read the state of the pushbutton value:
     buttonState[i] = digitalRead(buttonPin[i]);
-    allBtnState = String(allBtnState + "Btn" + i + ":" + buttonState[i] + "\t");
+    allBtnState = String(allBtnState + "Btn" + i + ":" + buttonState[i] + "," + buttonLock[i] + "\t");
     // https://www.arduino.cc/reference/en/language/variables/data-types/stringobject/
   }
   Serial.println(allBtnState);
 
   // check if the pushbutton is pressed.
   // if it is, the buttonState is HIGH:
-  if (buttonState[3] == LOW) {
+  for (int i = 1; i < 6; i++) {
+    if (buttonState[i] == LOW) {
+      //如果有任何按鈕被按下，其對應的接收腳位狀態為低電位
+      buttonLock[i] += 1;
+      if (buttonLock[i] == 69) {
+        //連續偵測到69次按下狀態，確認為人為操作非雜訊
+        Keyboard.write(48 + i);// ASCII code 48 represent '0';
+        Keyboard.write(10);// Send Enter
+        // The ASCII Code of ENTER KEY is 10 in Decimal or 0x0A in Hexadecimal.
+      }
+    } else {
+      buttonLock[i] = 0;
+      //該按鈕按下的狀態中斷，重新計算按下狀態
+    }
+  }
+
+  //test area
+  if (buttonState[3] == LOW) {//3號按鈕被按下
     // turn LED on:
     digitalWrite(ledPin, HIGH);
   }
