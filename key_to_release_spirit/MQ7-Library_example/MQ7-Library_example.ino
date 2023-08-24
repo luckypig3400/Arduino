d9c7777c7499dbaf938194c54ccf9481a11806ee
郵差 <twHenryYang@gmail.com>
2023-08-24 12:20:31 +0800

    feat: import MQ7-Library example code
    https://github.com/swatish17/MQ7-Library

---
+++ key_to_release_spirit/MQ7-Library_example/MQ7-Library_example.ino
@@ -0,0 +1,14 @@
+// REF: https://github.com/swatish17/MQ7-Library
+
+#include "MQ7.h"
+
+MQ7 mq7(A0,5.0);
+
+void setup() {
+    Serial.begin(9600);
+}
+
+void loop() {
+    Serial.println(mq7.getPPM());
+    delay(1000);
+}
