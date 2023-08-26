6abfee99f07b919ec0e6a17535e05bee19d49a06
郵差 <twHenryYang@gmail.com>
2023-08-26 12:37:36 +0800

    feat: 校準MQ9並輸出sensor讀值到OLEB面板

--- key_to_release_spirit/key_of_modules_calibration/key_of_modules_calibration.ino
+++ key_to_release_spirit/key_of_modules_calibration/key_of_modules_calibration.ino
@@ -32,22 +32,43 @@ int MQ7_A0_PIN = A0;
 int MQ7_D0_PIN = A1;
 // *******硬體腳位宣告區結尾*******
 
-double MQ9_R0 = 28.56;
-double MQ7_R0 = 5.27;
+double MQ9_R0 = 28.56;  //可能沒用
+double MQ7_R0 = 5.27;   //可能沒用
 
 void setup(void) {
   u8g2.begin();
-  Serial.begin(9600);
 }
 
 void loop(void) {
-  u8g2.setFont(u8g2_font_HelvetiPixelOutline_te);  //設定字型
+  String MQ9Voutput = "MQ9 Volt= ";
+  String MQ9R0output = "MQ9 R0= ";
+  float sensor_volt;
+  float RS_air;  //  Rs in clean air
+  float R0;      // R0 in 1000 ppm LPG
+  float sensorValue;
+
+  //Average
+  for (int x = 0; x < 100; x++) {
+    sensorValue = sensorValue + analogRead(MQ9_A0_PIN);
+  }
+  sensorValue = sensorValue / 100.0;
+  //-----------------------------------------------/
+
+  sensor_volt = (sensorValue / 1024) * 5.0;
+  RS_air = (5.0 - sensor_volt) / sensor_volt;  // Depend on RL on yor module
+  R0 = RS_air / 9.9;                           // According to MQ9 datasheet table
+  
+  MQ9Voutput += String(sensor_volt) += "mV";
+  MQ9R0output += String(R0);
+
+  u8g2.setFont(u8g2_font_profont15_tr);  //設定字型
   // 可用字型參考: https://github.com/olikraus/u8g2/wiki/fntlistall
   u8g2.firstPage();
   do {
-    u8g2.drawStr(0, 13, "Hello World !");  //輸出文字
-    u8g2.drawStr(0, 28, "CO: 10PPM");      //輸出文字
+    u8g2.drawStr(0, 13, MQ9Voutput.c_str());  //輸出文字
+    // https://forum.arduino.cc/t/how-to-display-a-string-variable-with-the-u8g2-library/622278/2
+    u8g2.drawStr(0, 26, MQ9R0output.c_str());      //輸出文字
     // u8g2.drawXBMP(0,16, imgWidth, imgHeight, logo_bmp);  //繪圖
   } while (u8g2.nextPage());
-  //delay(1000);
+  // delay(100);
 }
